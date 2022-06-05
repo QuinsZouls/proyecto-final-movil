dfd3ecbff779e594697ccd83b0f7f0dc8ae98583
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-06-05 18:29:13 -0500

    feature: add firebase env

---
+++ src/environment/firebase.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirestoreDB = getFirestore(FirebaseApp);
