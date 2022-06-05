b0e62927e26078a4421bb1a71b391f835c135afe
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-06-04 20:43:07 -0500

    bugfix: fix build compilation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,7 +19,7 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import './styles/index.scss';
+import '../styles/index.scss';
 setupIonicReact();
 
 const App: React.FC = () => (
