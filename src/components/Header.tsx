1a0b6bc45b20f8eefb4dea3f9f855c44ce3bcbaf
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-06-09 13:41:12 -0500

    ui: update header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,21 +9,34 @@ export interface HeaderProps {
   hideRightSide?: boolean;
   RightComponent?: React.ReactNode;
   LeftComponent?: React.ReactNode;
+  position?: 'block' | 'absolute' | 'fixed';
+  className?: string;
 }
-const Header: React.FC<HeaderProps> = ({ RightComponent, LeftComponent, hideRightSide }) => {
+const Header: React.FC<HeaderProps> = ({
+  RightComponent,
+  LeftComponent,
+  hideRightSide,
+  hideLeftSide,
+  position = 'block',
+  className = ''
+}) => {
   return (
-    <div className="header">
+    <div className={`header ${position} ${className}`}>
       <div className="navigation">
-        <div className="left-side">
-          {typeof LeftComponent !== 'undefined' ? (
-            LeftComponent
-          ) : (
-            <div onClick={() => history.goBack()}>
-              <IonIcon icon={arrowBackOutline} />
-            </div>
-          )}
-        </div>
-        <div className="right-side">{RightComponent}</div>
+        {!hideLeftSide ? (
+          <div className="left-side">
+            {typeof LeftComponent !== 'undefined' ? (
+              LeftComponent
+            ) : (
+              <div onClick={() => history.goBack()}>
+                <IonIcon icon={arrowBackOutline} />
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="left-side"></div>
+        )}
+        {!hideRightSide && <div className="right-side">{RightComponent}</div>}
       </div>
     </div>
   );
