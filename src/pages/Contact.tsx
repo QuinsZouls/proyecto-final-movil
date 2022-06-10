8b6907aa94c35cdfe6983150c11439dbad17c782
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-06-10 14:05:40 -0500

    feature: add contact screen

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import { IonIcon } from '@ionic/react';
+import { logoFacebook, logoTwitter, logoInstagram } from 'ionicons/icons';
+
 import Layout from '../components/Layout';
 
 const Contact: React.FC = () => {
   return (
     <Layout
       headerConfig={{
-        position: 'block',
+        position: 'absolute',
         hideRightSide: true
       }}
-      className="contact-screen"
+      className="home-screen"
     >
-      <div>Contact</div>
+      <div className="top-content">
+        <div className="title">
+          <h1>Encuentranos</h1>
+          <h5>Para ver más contenido</h5>
+        </div>
+      </div>
+      <div className="social-links">
+        <div className="link-wrapper">
+          <IonIcon icon={logoInstagram} className="instagram" />
+          <span>Finance_kids</span>
+        </div>
+        <div className="link-wrapper">
+          <IonIcon icon={logoTwitter} className="twitter" />
+          <span>Finance_kids</span>
+        </div>
+        <div className="link-wrapper">
+          <IonIcon icon={logoFacebook} className="facebook" />
+          <span>Finance_kids</span>
+        </div>
+      </div>
     </Layout>
   );
 };
