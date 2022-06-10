99380ed45b4f8388d2472d8cea3ec691214c767b
Anahi Buendia <kirbyanahi@gmail.com>
2022-06-10 01:50:14 -0500

    Pasos iniciales sección del curso

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
 import { useRouteMatch, useHistory } from 'react-router';
+import { IonCol, IonIcon, IonRow } from '@ionic/react';
+import { folder, time } from 'ionicons/icons';
 
 const Course: React.FC = () => {
   const history = useHistory();
@@ -16,13 +18,31 @@ const Course: React.FC = () => {
   return (
     <Layout
       headerConfig={{
-        position: 'block',
+        position: 'absolute',
         hideRightSide: true
       }}
       className="course-screen"
     >
-      <h1>Aquí va el contenido del curso, con la lista de las lecciones</h1>
-      <div>course detail: {courseId}</div>
+      <div className="top-content-course"></div>
+      <div className="main-content-course">
+        <h1>¡Empecemos!</h1>
+        <IonRow>
+          <IonIcon className="icon" icon={folder}></IonIcon>
+          <IonCol className="ion-align-self-center"> 4 Niveles</IonCol>
+          <IonIcon className="icon" icon={time}></IonIcon>
+          <IonCol className="ion-align-self-center">2-5 min | Nivel</IonCol>
+        </IonRow>
+        <p>Aquí aprenderas un poco sobre la historia del dinero</p>
+
+        <div className="course-lavel">
+          <h3>Aprendamos</h3>
+          <div className="circle">1</div>
+          <div className="circle">2</div>
+          <div className="circle">3</div>
+          <div className="circle">4</div>
+        </div>
+        <div>course detail: {courseId}</div>
+      </div>
     </Layout>
   );
 };
